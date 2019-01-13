52da0f568a54c6a8f6b988ebca2f73cee4bd8918
Gabe Horvat <realgabrielhorvat@gmail.com>
2019-01-13 12:02:59 +0100

    w/ pseudocode

--- reverse_words_in_str.rb
+++ reverse_words_in_str.rb
@@ -1,3 +1,8 @@
+# split string into characters
+# empty array for reversed string
+# as many times as elements in string, pop last element off and push it into empty array
+# join elements back together
+
 def reverse_string(string)
   split_string = string.split("")
   reversed = []
@@ -6,4 +11,3 @@ def reverse_string(string)
 end
 
 puts reverse_string("hello")
-
